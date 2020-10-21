455b97dc99a60886c0f7ede7dacbced05997463a
NenadM <nenad.manev@gmail.com>
2020-10-21 16:15:20 +0200

    modify controller

--- GymProject/Controllers/AccountController.cs
+++ GymProject/Controllers/AccountController.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
-using GymProject.Models;
-
-namespace GymProject.Controllers
+﻿namespace GymProject.Controllers
 {
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+    using GymProject.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.Owin.Security;
+
     [Authorize]
     public class AccountController : Controller
     {
